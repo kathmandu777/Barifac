cf65b68e24826075d803de5834118a44f82758e8
kathmandu777 <mahhakatomanato@gmail.com>
2021-11-23 01:13:04 +0900

    feat: add health check endpoint #12

--- fastapi/app/main.py
+++ fastapi/app/main.py
@@ -2,11 +2,13 @@ from fastapi import APIRouter, FastAPI
 
 from .middlwares import DBSessionMiddleware, HttpRequestMiddleware
 from .routers.v1 import api_v1_router
+from .routers.monitoring import monitoring_router
 
 app = FastAPI()
 app.add_middleware(DBSessionMiddleware)
 app.add_middleware(HttpRequestMiddleware)
 
 router = APIRouter()
+router.include_router(monitoring_router, tags=["monitoring"])
 router.include_router(api_v1_router, prefix="/api/v1", tags=["api/v1"])
 app.include_router(router)
