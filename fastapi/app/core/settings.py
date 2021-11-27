aaa9e1ae544109a6e8a46b4024fb0976aa431144
Arhikiyu <31932@toyota.kosen-ac.jp>
2021-11-27 18:35:21 +0900

    Fix#11

--- fastapi/app/core/settings.py
+++ fastapi/app/core/settings.py
@@ -16,7 +16,7 @@ class Settings(BaseSettings):
     db_port: int = 5432
     db_user: str = "postgres"
     db_password: str = "password"
-    dp_engine: str = "postgresql+psycopg2"
+    db_engine: str = "postgresql+psycopg2"
 
     # cors
     allow_origins: list = ["*"]
@@ -28,7 +28,7 @@ class Settings(BaseSettings):
     @property
     def db_url(self):
         s = Settings()
-        return f"postgresql+psycopg2://{s.db_user}:{s.db_password}@{s.db_host}:{str(s.db_port)}/{s.db_name}"
+        return f"{s.db_engine}://{s.db_user}:{s.db_password}@{s.db_host}:{str(s.db_port)}/{s.db_name}"
 
 
 @lru_cache
