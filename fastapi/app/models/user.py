474b5c3a3347ac244932d3a8686c258fe0b7de75
kathmandu777 <mahhakatomanato@gmail.com>
2021-12-04 21:21:26 +0900

    feat: add user schema validation #28

--- fastapi/app/models/user.py
+++ fastapi/app/models/user.py
@@ -1,15 +1,17 @@
 from sqlalchemy import BOOLEAN, VARCHAR, Column, String
 
+from ..core.constants import USERNAME_MAX_LENGTH
 from ..models import BaseModelMixin
 
 
 class User(BaseModelMixin):
     __tablename__ = "users"
 
-    username = Column(VARCHAR(256), unique=True, nullable=False)
-    email = Column(String, unique=True, nullable=False)
+    username = Column(VARCHAR(USERNAME_MAX_LENGTH), unique=True, nullable=False)
+    mail = Column(String, unique=True, nullable=False)
     uid = Column(VARCHAR(128), nullable=True)
     hashed_password = Column(String, nullable=True)
 
     is_admin = Column(BOOLEAN, nullable=False, default=False)
     is_active = Column(BOOLEAN, nullable=False, default=True)
+    is_not_active = Column(BOOLEAN, nullable=False, default=True)
