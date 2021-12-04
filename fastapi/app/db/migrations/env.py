4ecec683b140a2138083bcd51a8c7c834f936299
kathmandu777 <mahhakatomanato@gmail.com>
2021-12-04 23:28:14 +0900

    fix: user model

--- fastapi/app/db/migrations/env.py
+++ fastapi/app/db/migrations/env.py
@@ -16,6 +16,7 @@ fileConfig(config.config_file_name)
 # for 'autogenerate' support
 # from myapp import mymodel
 # target_metadata = mymodel.Base.metadata
+from app.models import *  # noqa
 
 target_metadata = Base.metadata
 
