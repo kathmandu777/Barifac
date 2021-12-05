11682158994b57beaa8d010331d6c9bb3cf63484
kathmandu777 <mahhakatomanato@gmail.com>
2021-12-05 15:46:56 +0900

    feat: add term model #41

--- fastapi/app/models/__init__.py
+++ fastapi/app/models/__init__.py
@@ -1,4 +1,5 @@
 from .base import BaseModelMixin
 from .department import Department
 from .school import School
+from .term import Term
 from .user import User
---
+++ fastapi/app/models/term.py
@@ -0,0 +1,12 @@
+from sqlalchemy import VARCHAR, Column, Integer
+from sqlalchemy.sql.schema import UniqueConstraint
+
+from ..models import BaseModelMixin
+
+
+class Term(BaseModelMixin):
+    __tablename__ = "terms"
+    __table_args__ = UniqueConstraint("academic_year", "semester"), {}
+
+    academic_year = Column(Integer, nullable=False)
+    semester = Column(VARCHAR(10), nullable=False)
