3354ca1fff9cc6c64d09d1ae2403693d13bdc3fd
kathmandu777 <mahhakatomanato@gmail.com>
2021-12-05 15:51:11 +0900

    fix: department model unique mistake #41

--- fastapi/app/models/department.py
+++ fastapi/app/models/department.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, ForeignKey, VARCHAR
+from sqlalchemy import VARCHAR, Column, ForeignKey
 from sqlalchemy.dialects.postgresql import UUID
 from sqlalchemy.schema import UniqueConstraint
 
@@ -9,7 +9,7 @@ class Department(BaseModelMixin):
     __tablename__ = "departments"
     __table_args__ = UniqueConstraint("name", "school_uuid"), {}
 
-    name = Column(VARCHAR(100), unique=True, nullable=False)
+    name = Column(VARCHAR(100), nullable=False)
     school_uuid = Column(
         UUID(as_uuid=True),
         ForeignKey("schools.uuid"),
