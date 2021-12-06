1a239510bdff07639afbfc89c0a0accdcce00f32
kathmandu777 <mahhakatomanato@gmail.com>
2021-12-06 16:34:12 +0900

    fix: declare UID_OR_PASSWORD_MUST_BE_SET

--- fastapi/app/crud/user.py
+++ fastapi/app/crud/user.py
@@ -1,5 +1,4 @@
-from xml.dom import ValidationErr
-
+from ..core.exceptions import UID_OR_PASSWORD_MUST_BE_SET, ApiException
 from ..core.security import get_password_hash
 from ..models import User
 from .base import BaseCRUD
@@ -16,7 +15,7 @@ class UserCRUD(BaseCRUD):
         elif data["uid"] is not None:
             return super().create(data)
         else:
-            raise ValidationErr("uid or password must be set.")
+            raise ApiException(UID_OR_PASSWORD_MUST_BE_SET)
 
     def update(self, obj: model, data: dict = {}) -> model:
         if data["password"] is not None:
@@ -26,7 +25,7 @@ class UserCRUD(BaseCRUD):
         elif data["uid"] is not None:
             return super().update(obj, data)
         else:
-            raise ValidationErr("uid or password must be set.")
+            raise ApiException(UID_OR_PASSWORD_MUST_BE_SET)
 
     def get_by_email(self, email: str) -> model:
         return self.get_query().filter_by(email=email).first()
