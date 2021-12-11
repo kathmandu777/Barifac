9614a4c3514c03bacdd7236c40c0e9f4a7cbf908
kathmandu777 <mahhakatomanato@gmail.com>
2021-12-12 00:23:11 +0900

    fix: add __init__ in schoolCRUD

--- fastapi/app/crud/school.py
+++ fastapi/app/crud/school.py
@@ -1,21 +1,24 @@
 from typing import Optional
 
+from sqlalchemy.orm import scoped_session
+
 from ..models import School
 from .base import BaseCRUD
 
 
 class SchoolCRUD(BaseCRUD):
-    model = School
+    def __init__(self, db_session: scoped_session):
+        super().__init__(db_session, School)
 
-    def get_by_name(self, name: str) -> Optional[model]:
+    def get_by_name(self, name: str) -> Optional[School]:
         return self.get_query().filter_by(name=name).first()
 
-    def create(self, data: dict = {}) -> model:
+    def create(self, data: dict = {}) -> School:
         name: str = data["name"]
         school = self.get_by_name(name)
         return school if school else super().create(data)
 
-    def update(self, obj: model, data: dict = {}) -> model:
+    def update(self, obj: School, data: dict = {}) -> School:
         name: str = data["name"]
         school = self.get_by_name(name)
         return school if school else super().update(obj, data)
