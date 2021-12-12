5bf6c1dafe69a18e38fe1ee382a0144d8858a6ba
kathmandu777 <mahhakatomanato@gmail.com>
2021-12-12 13:35:10 +0900

    fix: change return value type in UserAPI

--- fastapi/app/api/v1/user.py
+++ fastapi/app/api/v1/user.py
@@ -3,28 +3,27 @@ from uuid import UUID
 
 from app.core.exceptions import ApiException, create_error
 from app.crud import UserCRUD
-from app.schemas import CreateUserSchema, ReadUserSchema, UpdateUserSchema
+from app.models import User
+from app.schemas import CreateUserSchema, UpdateUserSchema
 
 from fastapi import Request
 
 
 class UserAPI:
     @classmethod
-    def gets(cls, request: Request) -> List[ReadUserSchema]:
+    def gets(cls, request: Request) -> List[User]:
         return UserCRUD(request.state.db_session).gets()
 
     @classmethod
-    def get(cls, request: Request, user_id: UUID) -> Optional[ReadUserSchema]:
+    def get(cls, request: Request, user_id: UUID) -> Optional[User]:
         return UserCRUD(request.state.db_session).get_by_uuid(user_id)
 
     @classmethod
-    def create(cls, request: Request, schema: CreateUserSchema) -> CreateUserSchema:
+    def create(cls, request: Request, schema: CreateUserSchema) -> User:
         return UserCRUD(request.state.db_session).create(schema.dict())
 
     @classmethod
-    def update(
-        cls, request: Request, uuid: UUID, schema: UpdateUserSchema
-    ) -> UpdateUserSchema:
+    def update(cls, request: Request, uuid: UUID, schema: UpdateUserSchema) -> User:
         obj = UserCRUD(request.state.db_session).get_by_uuid(uuid)
         if not obj:
             raise ApiException(
