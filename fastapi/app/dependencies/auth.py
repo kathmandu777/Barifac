2da7c5c919392ba8ac6cc58566e3ebf2a25eb512
kathmandu777 <mahhakatomanato@gmail.com>
2021-12-14 00:03:59 +0900

    fix: add depends in login required

--- fastapi/app/dependencies/auth.py
+++ fastapi/app/dependencies/auth.py
@@ -1,14 +1,21 @@
+from app.core.auth import OAuth2PasswordBearer
 from app.core.exceptions import ApiException, InvalidToken, PermissionDenied
 
-from fastapi import Request
+from fastapi import Depends, Request
+
+PASSWORD_LOGIN = OAuth2PasswordBearer(tokenUrl="/api/v1/auth/login/password")
 
 
-async def login_required(request: Request) -> None:
+async def login_required(
+    request: Request, password_login=Depends(PASSWORD_LOGIN)
+) -> None:
     if not request.user.is_authenticated:
         raise ApiException(InvalidToken)
 
 
-async def admin_required(request: Request) -> None:
+async def admin_required(
+    request: Request, password_login=Depends(PASSWORD_LOGIN)
+) -> None:
     if not request.user.is_authenticated:
         raise ApiException(InvalidToken)
     if not request.user.is_admin:
