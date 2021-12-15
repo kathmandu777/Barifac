26b756e1859f9bde30f7844233024e9c4ca1f5b1
yuta0709 <yuta0709dev@gmail.com>
2021-12-15 22:09:20 +0900

    fix: update repository #138

--- frontend/src/repositories/DepartmentRepository.ts
+++ frontend/src/repositories/DepartmentRepository.ts
@@ -11,11 +11,23 @@ export class DepartmentRepository {
   static async gets(schoolUuid: string) {
     const authClientObject = authClient();
     if (!authClientObject) return;
-    const res = await authClientObject.get<DepartmentObject>(
+    const res = await authClientObject.get<DepartmentObject[]>(
       `/api/v1/departments?school_uuid=${schoolUuid}`,
+    );
+    return res.data.map(department =>
+      DepartmentFactory.createFromResponseObject(department),
+    );
+  }
+
+  static async get(uuid: string) {
+    const authClientObject = authClient();
+    if (!authClientObject) return;
+    const res = await authClientObject.get<DepartmentObject>(
+      `/api/v1/departments/${uuid}}`,
     );
     return DepartmentFactory.createFromResponseObject(res.data);
   }
+
   public static async create({
     school,
     syllabus_url,
