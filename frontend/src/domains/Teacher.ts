d856bafb73a68ec99e12e3231e50ddc208cca829
yuta0709 <yuta0709dev@gmail.com>
2021-12-15 16:23:02 +0900

    fix: update models #128

---
+++ frontend/src/domains/Teacher.ts
@@ -0,0 +1,9 @@
+import { School } from '.';
+
+export class Teacher {
+  constructor(
+    public uuid: string,
+    public name: string,
+    public school: School,
+  ) {}
+}
---
+++ frontend/src/domains/TeacherComment.ts
@@ -0,0 +1,10 @@
+import { Teacher, User } from '.';
+
+export class TeacherComment {
+  constructor(
+    public uuid: string,
+    public teacher: Teacher,
+    public user: User,
+    public comment: string,
+  ) {}
+}
