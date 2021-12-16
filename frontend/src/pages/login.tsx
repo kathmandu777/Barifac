662c93ef1a1c0bfedf8eccbab535da2657b4c3f0
Dz0526 <this@dz99.me>
2021-12-16 09:04:17 +0900

    Feat(SignIn): 初回ログイン時にユーザ情報入力に遷移

--- frontend/src/pages/login.tsx
+++ frontend/src/pages/login.tsx
@@ -4,26 +4,23 @@ import { login } from 'libmod/firebase/auth';
 import { Heading, VStack } from '@chakra-ui/layout';
 import { NextPage } from 'next';
 import { UserRepository } from 'repositories/UserRepository';
+import router from 'next/router';
 
 const Login: NextPage = () => {
   // 動作確認（テスト書け）
-  const fetchUserData = async () => {
+  const redirectIfMissUserInfo = async () => {
     const user = await UserRepository.getMe();
-    console.log(user);
-    const update_user = await UserRepository.update({
-      username: 'Daiki',
-      email: user?.email,
-      uid: user?.uid,
-      grade: 3,
-      school_uuid: 'c0e31daf-987b-4f7a-87e8-22fec5a75c4d',
-      department_uuid: '703297de-e177-46f4-9093-767fe0fd81a3',
-    });
-    console.log(update_user);
+    {
+      console.log(user);
+      user && (!user.grade || !user.school || !user.department)
+        ? router.replace('/user/info')
+        : router.replace('/');
+    }
   };
 
   useEffect(() => {
-    fetchUserData();
-  }, []);
+    redirectIfMissUserInfo();
+  });
   return (
     <VStack spacing={10} mt={300}>
       <Heading as='h1' size='xl' color='blue.400' fontWeight='bold'>
